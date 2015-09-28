9e07205422a85e14ea489021f14d95a262139c71
Filip Hedman <hedman.filip@gmail.com>
2015-09-28 16:12:59 +0200

    Fixed compilation errors

--- server-st.c
+++ server-st.c
@@ -15,10 +15,11 @@
 #include "util.h"
 
 /* Structure of a HTTP request. */
-typedef struct {
-	int fd;
-	long size, arrival, dispatch;
-} request;
+struct Request {
+  int fd;
+  long size, arrival, dispatch;
+};
+typedef struct Request request;
 
 /**
  * Very simple input option parser.
@@ -29,22 +30,23 @@ typedef struct {
  */
 void getargs(int argc, char *argv[], int *port)
 {
-	/* TODO: Verify that the arguments are valid */
-    assert(port != NULL);
-    if (argv) {
-        if (argc == 1) {
-            *port = atoi(argv[0]);
-        }
-        else {
-            printf("Too many arguments! Please enter only the number of the port (above 2000).");
-        }
+  /* TODO: Verify that the arguments are valid */
+  assert(port != NULL);
+  if (argv) {
+    if (argc == 2) {
+      printf("arg: %d\n", atoi(argv[1]));
+      *port = atoi(argv[1]);
     }
+    else {
+      printf("Too many arguments! Please enter only the number of the port (above 2000).");
+    }
+  }
 }
 
 int requestcmp(const void *first, const void *second) {
-	assert(first != NULL);
-	assert(second != NULL);
-	return ((*(request **)first)->size - (*(request **)second)->size);
+  assert(first != NULL);
+  assert(second != NULL);
+  return ((*(request **)first)->size - (*(request **)second)->size);
 }
 
 /**
@@ -54,7 +56,7 @@ int requestcmp(const void *first, const void *second) {
  * @t: timeval structure
  */
 long calculate_time(struct timeval t) {
-	return ((t.tv_sec) * 1000 + t.tv_usec/1000.0) + 0.5;
+  return ((t.tv_sec) * 1000 + t.tv_usec/1000.0) + 0.5;
 }
 
 
@@ -62,42 +64,43 @@ long calculate_time(struct timeval t) {
  * Main server loop
  */
 int main(int argc, char *argv[]) {
-	int listenfd, connfd, port, clientlen;
-	struct sockaddr_in clientaddr;
-	struct timeval arrival;
+  int listenfd, connfd, port;
+  socklen_t clientlen;
+  struct sockaddr *clientaddr = NULL;
+  struct timeval arrival;
 
-	/* Parse the input arguments */
-	getargs(argc, argv, &port);
+  /* Parse the input arguments */
+  getargs(argc, argv, &port);
 
-	printf("\n1DT032 server: Hello! I am running on port %d.\n\n", port);
+  printf("\n1DT032 server: Hello! I am running on port %d.\n\n", port);
 
-	/* Listen to user selected port */
-	listenfd = Open_listenfd(port);
+  /* Listen to user selected port */
+  listenfd = Open_listenfd(port);
 	
-	/* Main server loop */
-	while (1) {
-		clientlen = sizeof(clientaddr);
+  /* Main server loop */
+  while (1) {
+    clientlen = sizeof(clientaddr);
 
-		/* TODO: Accept a connection and retrieve connfd */
-        connfd = Accept(listenfd,clientaddr,clientlen);
+    /* TODO: Accept a connection and retrieve connfd */
+    connfd = Accept(listenfd,clientaddr, &clientlen);
 
-		/* TODO: Allocate a request structure */
-        struct request req;
+    /* TODO: Allocate a request structure */
+    request req;
         
-		/* TODO: Save the time for the statistics */
-        gettimeofday(&arrival, NULL);
+    /* TODO: Save the time for the statistics */
+    gettimeofday(&arrival, NULL);
         
-		/* TODO: Set the request file descriptor to the one accepted */
-        req.fd = listenfd;
+    /* TODO: Set the request file descriptor to the one accepted */
+    req.fd = listenfd;
         
-		/* TODO: Set the arrival and dispatch time */
-        req.arrival = calculate_time(arrival);
-        req.dispatch = calculate_time(arrival);
+    /* TODO: Set the arrival and dispatch time */
+    req.arrival = calculate_time(arrival);
+    req.dispatch = calculate_time(arrival);
         
-		/* TODO: Call the request handler */
-        requestHandle(req.fd,req.arrival,req.dispatch);
+    /* TODO: Call the request handler */
+    requestHandle(req.fd,req.arrival,req.dispatch);
         
-		/* TODO: Close */
-        Close(req.fd);
-	}
+    /* TODO: Close */
+    Close(req.fd);
+  }
 } 
